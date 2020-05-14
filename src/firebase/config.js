4b1fe56f8e7b7e3ce60857c28db417eb72715d37
imamurasyoutarou <imasyou718@icloud.com>
2020-05-14 21:10:49 +0900

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCcQFkxizR7GWXNp_ZxfRYRBKzb_N1sASk",
+  authDomain: "testbot-3db30.firebaseapp.com",
+  databaseURL: "https://testbot-3db30.firebaseio.com",
+  projectId: "testbot-3db30",
+  storageBucket: "testbot-3db30.appspot.com",
+  messagingSenderId: "367704491211",
+  appId: "1:367704491211:web:d60c3613c0ce53a226074e",
+};
+export default firebaseConfig;
